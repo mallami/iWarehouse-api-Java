67eef1d73c0ccd01c332fee2ab15cd42aa2b4449
mallami <71060267+mallami@users.noreply.github.com>
2021-09-25 23:17:55 -0500

    Add files via upload

---
+++ iWarehouse-api/src/main/java/com/iwarehouse/service/UserService.java
@@ -0,0 +1,83 @@
+package com.iwarehouse.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.iwarehouse.model.User;
+import com.iwarehouse.repository.UserRepo;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+	
+	private UserRepo userRepo;
+	
+	// ADD NEW USER
+	public void addUser(User user) {
+		user.setEmail(user.getEmail().toLowerCase());
+		userRepo.save(user);
+	}
+	
+	// GET ALL USERS
+	public List<User> getAllUsers() {
+		return userRepo.getAllUsers();
+	}
+	
+	// GET INACTIVE USERS
+	public List<User> getInactiveUsers() {
+		return userRepo.getInactiveUsers();
+	}
+	
+	// GET ACTIVE USERS
+	public List<User> getActiveUsers() {
+		return userRepo.getActiveUsers();
+	}	
+	
+	// GET USER BY userID
+	public User getUserById(int userId) {
+		return userRepo.getUserById(userId);
+	}
+	
+	// GET USER BY FIRST firstNAME (OR CONTAINS PART OF THE NAME)
+	public List<User> getUserByName(String firstName) {
+		return userRepo.getUserByName(firstName);
+	}
+	
+	// GET USER BY USERNAME - ACTIVE ONLY
+	public User getUserByUsername(String username) {
+		return userRepo.getUserByUsername(username);
+	}
+	
+	// USER LOGIN
+	public Boolean userLogin(String username, String password) {
+		boolean resp = false;
+		User user = getUserByUsername(username);
+		if(user != null) {
+			if(user.getEmail().equalsIgnoreCase(username) & user.getPassword().equals(password)) {
+				resp = true;
+			}
+		}
+		
+		return resp;
+	}
+	
+	// CHANGE PASSWORD BY userID
+	public void changePassword(String password, String email) {
+		userRepo.changePassword(password, email);
+	}
+	
+	// UPDATE USER
+	public void updateUser(User user) {
+		user.setEmail(user.getEmail().toLowerCase());
+		userRepo.save(user);
+	}
+	
+	// DELETE USER By userID
+	public void deleteUser(int userId) {
+		userRepo.deleteUser(userId);
+	}
+
+}
